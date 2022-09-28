6779851dbe482e8210d11570f963fef6a1e9de3b
WouterZandsteeg <112496208+Raveleijn@users.noreply.github.com>
2022-09-28 20:57:08 +0000

    fixed heapsort

--- heapsort.py
+++ heapsort.py
@@ -2,45 +2,77 @@ import math
 
 from randomListGenerator import *
 
-randoml = randomList(10,0,9)
-print(randoml)
 
 def Heapify(list):
   for i in range(len(list)):
-    Sieve(list, len(list)-i-1)
+    Sieve(list, len(list)-i-1, len(list))
   return(list)
 
 
-def Sieve(list, index):
-  element = list[index]
-  if len(list)>2*index+1:
-    if list[index]>list[2*index+1]:
+def Sieve(list, index, length):
+  parent = list[index]
+  children = 0
+  if length > 2*index+1:
+    child1 = list[2*index+1]
+    children += 1
+  if length > 2*index+2:
+    child2 = list[2*index+2]
+    children += 1
+
+  if children == 1:
+    if child1>parent:
       list[index], list[2*index+1] = list[2*index+1], list[index]
-  
-  if len(list)>2*index+2:
-    if list[index]>list[2*index+2]:
-      list[index], list[2*index+2] = list[2*index+2], list[index]
-  
-  if element != list[index]:
-    if element == list[2*index+1]:
-      Sieve(list, 2*index+1)
+      Sieve(list, 2*index+1, length)
+
+  if children == 2:
+    if child1>child2:
+      if child1>parent:
+        list[index], list[2*index+1] = list[2*index+1], list[index]
+        Sieve(list, 2*index+1, length)
     else:
-      Sieve(list, 2*index+2)
+      if child2>parent:
+        list[index], list[2*index+2] = list[2*index+2], list[index]
+        Sieve(list, 2*index+2, length)
+  return list
 
 def SortHeap(heap):
-  sortedList = []
-  while len(heap) != 0:
-    sortedList.append(heap[0])
-    heap[0] = heap[-1]
-    heap.pop(-1)
-    if(len(heap) != 0):
-      Sieve(heap, 0)
-  return sortedList
+  lengthHeap = len(heap)
+  while lengthHeap != 0:
+    heap[lengthHeap-1], heap[0] = heap[0], heap[lengthHeap-1]
+    lengthHeap -= 1
+    Sieve(heap, 0, lengthHeap)
+  return heap
 
-heapTest = Heapify(randoml)
-print(heapTest)
-sortedTest = SortHeap(heapTest)
-print(sortedTest)
+def TestHeap(heap):
+  correct = True
+  for i in range(len(heap)):
+    if len(heap)>2*i+1:
+      if heap[i]<heap[2*i+1]:
+        correct = False
+        break;
+    if len(heap)>2*i+2:
+      if heap[i]<heap[2*i+2]:
+        correct = False
+        break
+  return correct
+
+def TestSorted(list):
+  sorted = True
+  for i in range(len(list)-1):
+    if list[i]>list[i+1]:
+      sorted = False
+      break
+  return sorted
+    
+
+for i in range(100):
+  randoml = randomList(100,0,99)
+  Heapify(randoml)
+  SortHeap(randoml)
+  print(TestSorted(randoml))
+
+
+  
       
       
     
