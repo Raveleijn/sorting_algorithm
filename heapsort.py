df7d7db417eea7c8f643dacfd02798a0513c50af
WouterZandsteeg <112496208+Raveleijn@users.noreply.github.com>
2022-09-28 09:44:43 +0000

    started making heapsort

---
+++ heapsort.py
@@ -0,0 +1,12 @@
+import math
+
+def Heapify(list):
+  heap = []
+  for i in list:
+    inserting = True
+    treePos = len(heap)
+    heap.append(i)
+    while inserting:
+      parentPos = math.floor(treePos-1)
+      if 
+    
