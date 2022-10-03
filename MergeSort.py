8c4ee496ba6263671a392a55e3f9dab1db6631a6
Pepijnvb <114066541+PepijnvanB@users.noreply.github.com>
2022-10-03 12:56:12 +0000

    merge

---
+++ MergeSort.py
@@ -0,0 +1,53 @@
+#we gaan hier een algoritme maken dat eenllijst sorteert met de methode van merge sort
+
+List = [3,5,7,9,1,0,1]
+
+
+def mergesort(List):
+    if (len(List) > 16):
+        return
+    print(len(List))
+    if len(List) == 1:
+        return List
+    print(List)
+    ListA, ListB = splitlist(List)
+    print(ListA, ListB)
+    ListASorted = mergesort(ListA)
+    ListBSorted = mergesort(ListB)
+    ListSorted = merge(ListASorted, ListBSorted)
+    return ListSorted
+
+
+def splitlist(list):
+    i = 0
+    ListA = []
+    ListB = []
+    while i < len(list) / 2:
+        ListA.append(list[i])
+        i += 1
+    while i >= (len(list) / 2) and i < len(list):
+        ListB.append(list[i])
+        i += 1
+    return ListA, ListB
+
+
+def merge(ListASorted, ListBSorted):
+  ListSorted = []
+  while (len(ListASorted) != 0) and (len(ListBSorted) != 0):
+    if ListASorted[0] <= ListBSorted[0]:
+      ListSorted.append(ListASorted[0]) 
+      ListASorted.pop(0)
+    elif ListASorted[0] > ListBSorted[0]:
+      ListSorted.append(ListBSorted[0]) 
+      ListBSorted.pop(0)
+  while (len(ListASorted) == 0) ^ (len(ListBSorted) == 0):
+    while (len(ListASorted) == 0) and (len(ListBSorted) != 0):
+      ListSorted.append(ListBSorted[0])
+      ListBSorted.pop(0)
+    while (len(ListBSorted) == 0) and (len(ListASorted) != 0):
+      ListSorted.append(ListASorted[0])
+      ListASorted.pop(0)
+  return ListSorted
+
+
+print(mergesort(List))
