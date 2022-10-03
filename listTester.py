2ac780ccd2d27ca02aa8cc18d078a419549791f5
pepinus <114067264+SungJinwooh@users.noreply.github.com>
2022-10-03 12:54:35 +0000

    bubbles

--- listTester.py
+++ listTester.py
@@ -1,14 +1,20 @@
 from randomListGenerator import *
 from heapsort import *
+from bubblesort import *
 
 sort  = input('heapsort, bubblesort, mergesort')
-if sort == heapsort:
+if sort == 'heapsort':
   for i in range(1):
-  randoml = randomList(10,0,9)
-  print(randoml)
-  Heapify(randoml)
-  print(randoml)
-  SortHeap(randoml)
-  print(randoml)
-  print(str(i)+': '+str(TestSorted(randoml)))
-
+    randoml = randomList(10,0,9)
+    print(randoml)
+    Heapify(randoml)
+    print(randoml)
+    print(randoml)
+    print(str(i)+': '+str(TestSorted(randoml)))
+if sort == 'bubblesort':
+  lijst = invertedLijst(10)
+  print(lijst)
+  bubbleSort(lijst)
+  print(lijst)
+  print(TestSorted(lijst))
+  
