ad8822231f27f7389768a1f38783157d2eb30020
WouterZandsteeg <112496208+Raveleijn@users.noreply.github.com>
2022-10-05 10:41:23 +0000

    fixed bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,11 @@
 def bubbleSort(arr):
   LengteArray = len(arr)
-  wissel = False
   for i in range(LengteArray-1):
+    wissel = False
     for j in range(0, LengteArray-i-1):
       if arr[j] > arr[j + 1]:
         wissel = True
         arr[j], arr[j + 1] = arr[j + 1],  arr[j]
          
-        if not wissel:
-          return arr
+    if not wissel:
+      return arr
