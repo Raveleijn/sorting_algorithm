ec5dd267deff164764a3aa0f4cdb1f0ea09c46ad
Pepijnvb <114066541+PepijnvanB@users.noreply.github.com>
2022-10-05 09:56:04 +0000

    comments

--- MergeSort.py
+++ MergeSort.py
@@ -1,5 +1,6 @@
 #we gaan hier een algoritme maken dat eenllijst sorteert met de methode van merge sort
 
+#Hier maken we gebruik van een recursieve functie om te blijven spitten en daarna mergen totdat de lijst helemaal gesorteerd is
 def mergesort(List):
     if (len(List) > 16):
         return
@@ -11,7 +12,7 @@ def mergesort(List):
     ListSorted = merge(ListASorted, ListBSorted)
     return ListSorted
 
-
+#Hier wordt de lijst gesplitst in twee lijsten met ieder de helft van de getallen
 def splitlist(list):
     i = 0
     ListA = []
@@ -24,7 +25,7 @@ def splitlist(list):
         i += 1
     return ListA, ListB
 
-
+#Hier voegen we twee lijsten samen door steeds het eerste getal van beide lijsten met elkaar te vergelijken en de laagste aan een derde lijst toe te voegen, waarna ik dat getal verwijder uit de eerste lijst en er weer een nieuw eerste getal ontstaat.
 def merge(ListASorted, ListBSorted):
   ListSorted = []
   while (len(ListASorted) != 0) and (len(ListBSorted) != 0):
