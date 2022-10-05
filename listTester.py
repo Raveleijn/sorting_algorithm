a63bc0564aa287b54ced62068d7efd3f297e801b
WouterZandsteeg <112496208+Raveleijn@users.noreply.github.com>
2022-10-05 09:56:05 +0000

    listtester woohoo

--- listTester.py
+++ listTester.py
@@ -3,30 +3,25 @@ from NearlySorted import *
 from heapsort import *
 from bubblesort import *
 from MergeSort import *
+from InvertedLijst import *
 
-sortType  = input('What sorting algorithm do you want to use? (heapsort, bubblesort, mergesort) \n ')
-listType  = input('What list type do you want to use? (random, nearly sorted, inversed) \n ')
-length = str(input('What length? \n'))
+sortType  = input('What sorting algorithm do you want to use? (heapsort, bubblesort, mergesort) \n')
+listType  = input('What list type do you want to use? (random, nearly sorted, inversed) \n')
+length = int(input('What length? \n'))
+
+sortDict = {'heapsort': HeapSort,
+            'bubblesort': bubbleSort,
+            'mergesort': mergesort}
 
 listDict = {'random': RandomList,
             'nearly sorted': NearlySorted,
             'inversed': invertedLijst} 
 
-list = listDict['listType'](length)
+list = listDict[listType](length)
+sortFunc = sortDict[sortType]
 
+print(list)
+list = sortFunc(list)
+print(list)
+print(TestSorted(list))
 
-if sort == 'heapsort':
-  for i in range(1):
-    print(list)
-    Heapify(list)
-    print(list)
-    SortHeap(list)
-    print(list)
-    print(str(i)+': '+str(TestSorted(list)))
-if sort == 'bubblesort':
-  lijst = invertedLijst(10000)
-  print(lijst)
-  bubbleSort(lijst)
-  print(lijst)
-  print(TestSorted(lijst))
-  
--- randomListGenerator.py
+++ randomListGenerator.py
@@ -1,8 +1,9 @@
 #import the random library
 import random
 
-def RandomList(size=10000, min=0, max=9999):
-  numbersLeft = list(range(min, max+1))                      #creates a list of numbers to choose from
+def RandomList(size=10000, minVal=0, maxVal=9999):
+  maxVal = max(size-1, maxVal)
+  numbersLeft = list(range(minVal, maxVal+1))                      #creates a list of numbers to choose from
   randList = []                                              #the list we're going to add ranodm values to
   for i in range(size):
     randNumberIndex = random.randint(0, len(numbersLeft)-1)  #choose a random index for the new random number
