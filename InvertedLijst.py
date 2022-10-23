cbe3227b58667160812513df63b5a86d8adc2045
SungJinwooh <114067264+SungJinwooh@users.noreply.github.com>
2022-10-23 20:36:09 +0200

    Update InvertedLijst.py
    added explanations to inverted list generator

--- InvertedLijst.py
+++ InvertedLijst.py
@@ -1,9 +1,9 @@
 from random import randrange
 
-def invertedLijst(length):
+def invertedLijst(length): #de lenght van de lijst geef je aan bij het selecteren van welke lijst
   InvertedLijst = []
   x = 0
-  while len(InvertedLijst) < length:
-    x += randrange(1, 10)
-    InvertedLijst.append( 6*length - x)
-  return InvertedLijst
+  while len(InvertedLijst) < length: #zorgt ervoor dat er precies zoveel getallen in de lijst zitten als aangegeven
+    x += randrange(1, 10) #geeft een beetje variatie aan de getallen zodat het niet gewoon een reeks van precies aflopende getallen
+    InvertedLijst.append( 6*length - x) #zorgt ervoor dat er genoeg getallen zijn om te genereren en vormt stap voor stap de aflopende lijst, als hier geen 6* stond was er namelijk lenght-x en zou je op negatieve resultaten uitkomen in je lijst
+  return InvertedLijst #geeft je de inverted lijst terug
